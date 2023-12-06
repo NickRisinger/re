6a9241fc5ececa77d7d6659534d37f4c2549f79c
NickRisinger <boozy044@gmail.com>
2023-12-06 22:44:43 +0300

    update agencies controller

--- src/modules/agencies/agencies.controller.ts
+++ src/modules/agencies/agencies.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Patch, Post, Put } from '@nestjs/common';
 import { AgenciesService } from './agencies.service';
 
 @Controller('agencies')
 export class AgenciesController {
   constructor(private readonly agenciesService: AgenciesService) {}
+
+  @Post()
+  getAll() {}
+
+  @Get(':id')
+  getOne() {}
+
+  @Put('create')
+  create() {}
+
+  @Patch(':id')
+  update() {}
 }
