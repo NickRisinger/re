1dc4c74066395166edd2025c555d51bcb4a01f23
NickRisinger <boozy044@gmail.com>
2023-12-06 21:48:15 +0300

    update user entity

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,4 +1,9 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
@@ -25,4 +30,47 @@ export class User {
 
   @Column({ nullable: true })
   secondName: string;
+
+  @Column({ nullable: true })
+  advertFio: string;
+
+  @Column({ nullable: true })
+  phone: string;
+
+  @Column({ nullable: true })
+  dopPhone: string;
+
+  @Column({ nullable: true })
+  advertPhone: string;
+
+  @Column()
+  city: string;
+
+  // relations columns
+
+  // bolean columns
+  @Column({ nullable: true })
+  isOnline: boolean;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @Column({ default: false })
+  isDeleted: boolean;
+
+  // time columns
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Column({ nullable: true })
+  trainingAt: Date;
+
+  @Column({ nullable: true })
+  startWorkAt: Date;
+
+  @Column({ nullable: true })
+  dismissalAt: Date;
+
+  @Column({ nullable: true })
+  archivedAt: Date;
 }
